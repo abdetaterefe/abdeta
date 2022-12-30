3971ff37a149a75720a4fda075b9a2daf4602fdc
Abdi Terefe <abdetaterefe@gmail.com>
2022-12-30 18:43:16 +0300

    gtag

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'G-Y2XZFTTT2F', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y2XZFTTT2F">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'G-Y2XZFTTT2F');
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,9 @@
 	import '../app.postcss';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+	import Analytics from '$lib/analytics.svelte'
 </script>
-
+<Analytics/>
 <Header />
 
 <main class="p-4 md:px-10 md:py-10">
