7439f7d5c86e75c43ed1db1f80c8e88b7dd2aaad
Abdi Terefe <abdetaterefe@gmail.com>
2023-01-09 19:45:27 +0300

    rounded

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -11,7 +11,7 @@
 	<div class="p-2">
 		<button
 			on:click={handleToggle}
-			class="h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600"
+			class="rounded h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600"
 		>
 			<span class="font-bold text-xl">Close</span>
 		</button>
@@ -20,21 +20,21 @@
 				<a
 					href="/blog"
 					on:click={handleToggle}
-					class="h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
+					class="rounded h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
 				>
 					<span class="decoration-inherit no-underline font-bold text-xl">Blog</span>
 				</a>
 				<a
 					href="/project"
 					on:click={handleToggle}
-					class="h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
+					class="rounded h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
 				>
 					<span class="decoration-inherit no-underline font-bold text-xl">Projects</span>
 				</a>
 				<a
 					href="/uses"
 					on:click={handleToggle}
-					class="h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
+					class="rounded h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
 				>
 					<span class="decoration-inherit no-underline font-bold text-xl">Uses</span>
 				</a>
@@ -44,17 +44,19 @@
 </Drawer>
 
 <header class="p-4 flex flex-wrap justify-between">
-	<a class="text-lg justify-center items-center flex" href="/"><img class="rounded-lg" src="/favicon.ico" alt="Abdeta Logo"></a>
-	<button class="inline-flex items-center p-2 md:hidden border-2 border-myBlue-800">
+	<a class="text-lg justify-center items-center flex" href="/"
+		><img class="rounded-lg" src="/favicon.ico" alt="Abdeta Logo" /></a
+	>
+	<button class="rounded inline-flex items-center p-2 md:hidden border-2 border-myBlue-800">
 		<span class="sr-only">Open main menu</span>
 		<svg
 			class="w-6 h-6"
 			aria-hidden="true"
-			fill="currentColor"
 			viewBox="0 0 20 20"
 			xmlns="http://www.w3.org/2000/svg"
 			on:click={handleToggle}
 			><path
+				class="fill-black"
 				fill-rule="evenodd"
 				d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
 				clip-rule="evenodd"
@@ -65,19 +67,19 @@
 		<ul class="flex gap-4 list-none">
 			<a
 				href="/blog"
-				class="h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
+				class="rounded h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
 			>
 				<span class="decoration-inherit no-underline font-bold text-xl">Blog</span>
 			</a>
 			<a
 				href="/project"
-				class="h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
+				class="rounded h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
 			>
 				<span class="decoration-inherit no-underline font-bold text-xl">Projects</span>
 			</a>
 			<a
 				href="/uses"
-				class="h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
+				class="rounded h-10 bg-myBlue-800 text-white w-40 flex justify-center items-center hover:bg-myBlue-600 mb-5"
 			>
 				<span class="decoration-inherit no-underline font-bold text-xl">Uses</span>
 			</a>
