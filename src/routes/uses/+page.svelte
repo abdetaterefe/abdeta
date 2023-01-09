314eb2d6eed7521c977147aa3fda3f7784d1a60f
Abdi Terefe <abdetaterefe@gmail.com>
2023-01-09 20:45:07 +0300

    uses

--- src/routes/uses/+page.svelte
+++ src/routes/uses/+page.svelte
@@ -16,165 +16,30 @@
 		content="https://avatars.githubusercontent.com/u/59355292?s=400&u=9e89eb1ae60b57ed7c0ef0de4858640ae75ce22d&v=4"
 	/>
 </svelte:head>
-
+<h1 class="mb-4">
+	<strong class="text-myBlue-800">Uses</strong>
+</h1>
 <div class="grid md:grid-cols-2 gap-2">
 	<div>
-		<div>
-			<h1
-				class="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-			>
-				[ Development ]
-			</h1>
-			<div class="ml-2">
-				<div>
-					<h1
-						class="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-					>
-						Languages and frameworks
-					</h1>
-					<ul class="ml-2 mb-2 space-y-2">
-						<li>
-							<strong>Svelte</strong>: I've been working with it for the past six or more months,
-							and it served as my first introduction to the world of Javascript frameworks and
-							libraries. Since then, I've tried other ones, but I keep returning to Svelte and
-							Sveltekit.
-						</li>
-						<li>
-							<strong>Typescript</strong>: every project, whether it is a little project or
-							libraries I always start with typescript; I don't have a lot of experience with
-							javascript, but typescript is helping me understand it every day.
-						</li>
-						<li>
-							<strong>Tailwind</strong>: Without a tailwindcss, I am completely unable to build
-							anything, and it is very simple. Following the creation of any project, my first
-							action is to install Tailwind.
-						</li>
-					</ul>
-				</div>
-				<div>
-					<h1
-						class="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-					>
-						Code Tools
-					</h1>
-					<ul class="ml-2 mb-2 space-y-2">
-						<li>
-							<strong>Vs code</strong>: My preferred code editor is vs Code, but I have also used
-							and occasionally still use sublime.
-							<ul class="ml-4 mb-2 space-y-2">
-								<li>
-									<strong>Theme</strong>: Github Dark
-								</li>
-								<li>
-									<strong>Extensions</strong>: (some of my default everyday extensions) are:
-									Background by shalldie (i like put anime background on vscode), Code Runner, Git
-									History, GitHub Theme, Material Icon Theme, Live Server, Prettier, Svelte,
-									Tailwind, Thunder Client.
-								</li>
-							</ul>
-						</li>
-						<li>
-							<strong>Vercel</strong>: Nothing was lacking that I needed for me to use a different
-							option.
-						</li>
-						<li>
-							<strong>Chrome</strong>: Because I use a Chromebook as my primary development tool, it
-							comes pre-installed and is excellent. On my second, older Windows computer, I use
-							Firefox and Edge. I don't even put Chrome on.
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div>
-			<h1
-				class="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-			>
-				[ This site ]
-			</h1>
-			<ul class="ml-2 mb-2 space-y-2">
-				<li>
-					<strong>built with</strong>: sveltekit, tailwind, mdsvex, and deployed on vercel
-				</li>
-				<li>
-					<strong>Detail</strong>: It is static on the home page and this page, and it fetchs
-					markdown for the blog page. For markdown, I used mdsvex, and for the project page, it uses
-					a octokit to fetch the project from my github repositories.
-				</li>
-			</ul>
-		</div>
+		<h1 class="font-bold text-xl text-myBlue-800">Hardware and Software</h1>
+
+		<ul class="ml-4 space-y-2">
+			<li><a href="/">Lenovo Chromebook S340-14</a></li>
+			<li><a href="/">Lenovo A6 Note</a></li>
+			<li><a href="/">VsCode</a></li>
+			<li><a href="/">Chrome</a></li>
+			<li><a href="/">Zsh</a></li>
+		</ul>
 	</div>
 	<div>
-		<div>
-			<h1
-				class="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-			>
-				[ Hardware ]
-			</h1>
-			<div class="ml-2">
-				<div>
-					<h1
-						class="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-					>
-						PC
-					</h1>
-					<ul class="ml-2 mb-2 space-y-2">
-						<li>
-							<strong>Lenovo chromebook S340-14</strong>: I don't have the luxury to buy or have a
-							laptop that I want, it is the one that I got, I hate and also love it sometimes, I
-							hate it when programming, and sometimes I get angry to the point I want to punch it
-							through the screen. is not suitable for development purposes. it 4GB RAM and 32GB ROM.
-							it is snappy and fast until sometimes when nodejs starts to node. the Linux virtual
-							crashes and I have to shut it down and start it again if it isn't for my CTRL+S habit
-							and vs code not forgetting changes. I would have thrown that punch. and I love it when
-							browsing the web
-						</li>
-						<li>
-							<strong>Windows</strong>: Linux and Windows both tempt me, but after using WSL,
-							Windows has become my preferred operating system.
-						</li>
-					</ul>
-				</div>
-				<div>
-					<h1
-						class="font-bold text-xl text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-					>
-						Phone
-					</h1>
-					<ul class="ml-2 mb-2 space-y-2">
-						<li>
-							<strong>Lenovo A6 Note</strong>: it is the one that I got, and the spec is 3GB RAM and
-							32GB ROM.
-						</li>
-						<li>
-							<strong>Android</strong>: I prefer Android.
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div>
-			<h1
-				class="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-			>
-				[ Productivity & Utility ]
-			</h1>
-			<ul class="ml-2 mb-2 space-y-2">
-				<li>
-					<strong>Google Keep</strong>: for note-taking and saving checked lists.
-				</li>
-				<li>
-					<strong>Dashlane</strong>: for a password manager and I like autofill on apps on my phone
-					and on the web I use google password manager.
-				</li>
-				<li>
-					<strong>Nyx Music Player</strong>: I am not a Spotify or online music listener, I download
-					music locally on my phone, when it comes to local music players Nyx is the best.
-				</li>
-				<li>
-					<strong>Telegram</strong>: in Ethiopia, you can not live without telegram.
-				</li>
-			</ul>
-		</div>
+		<h1 class="font-bold text-xl text-myBlue-800">Productivity</h1>
+
+		<ul class="ml-4 space-y-2">
+			<li><a href="/">Google Keep</a></li>
+			<li><a href="/">Google Docs</a></li>
+			<li><a href="/">Dashlane</a></li>
+			<li><a href="/">Nyx Music Player</a></li>
+			<li><a href="/">Telegram</a></li>
+		</ul>
 	</div>
 </div>
