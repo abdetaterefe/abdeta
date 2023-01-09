90d2b6604cf75ac00c51fab4de1493b0415fcb51
Abdi Terefe <abdetaterefe@gmail.com>
2023-01-09 20:09:26 +0300

    blog page updated

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -4,11 +4,7 @@
 </script>
 
 <h1>
-	<strong
-		class="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900"
-	>
-		[ Blogs ]
-	</strong>
+	<strong class="text-myBlue-800">Blogs</strong>
 </h1>
 <div class="mt-4 grid grid-cols-4 gap-2">
 	<div class="hidden md:block">
@@ -16,16 +12,16 @@
 			type="text"
 			id="default-input"
 			placeholder="Search post titles"
-			class="border border-gray-300 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
+			class="rounded border border-gray-300 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
 		/>
 		<div class="mt-4">
-			<h1>CATEGORIES</h1>
+			<h1 class="mb-2">CATEGORIES</h1>
 
 			<div class="w-full" role="group">
 				{#each data.allCategories as category}
 					<a
 						href="/blog/category/{category}"
-						class="inline-flex mr-2 mb-2 items-center py-2 px-2 text-xs font-medium bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700"
+						class="inline-flex mr-2 rounded text-myBlue-800 items-center py-2 px-2 text-xs font-bold bg-myBlue-400 hover:bg-myBlue-300"
 						>{category}
 					</a>
 				{/each}
