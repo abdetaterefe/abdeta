31fd5f7696028fed1315b5a04bfedbe46323bd7b
Abdi Terefe <abdetaterefe@gmail.com>
2024-02-22 10:58:31 +0300

    footer overflow fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,12 @@
 	import Footer from '$lib/components/Footer.svelte';
 </script>
 
-<div class="dark:bg-zinc-900">
+<div class="dark:bg-zinc-900 flex flex-col h-screen overflow-hidden">
 	<Header />
-	<main class="p-4 md:px-10 md:py-10 min-h-[75vh]">
-		<slot />
-	</main>
+	<div class="flex-1 overflow-y-scroll">
+		<main class="p-4 md:px-10 md:py-10 min-h-[75vh]">
+			<slot />
+		</main>
+	</div>
 	<Footer />
 </div>
