1f025d9302400358cbd599861d6ef6d27889a924
Abdi Terefe <abdetaterefe@gmail.com>
2024-03-05 20:52:01 +0300

    footer fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,10 +6,10 @@
 
 <div class="dark:bg-zinc-900 flex flex-col h-screen overflow-hidden">
 	<Header />
-	<div class="flex min-h-[calc(100vh-4rem)] flex-col overflow-scroll">
-		<div class="flex-grow p-4 md:px-10 md:py-10">
+	<div class="flex-1 overflow-y-scroll">
+		<main class="p-4 md:px-10 md:py-10">
 			<slot />
-		</div>
-		<Footer />
+		</main>
 	</div>
+	<Footer />
 </div>
